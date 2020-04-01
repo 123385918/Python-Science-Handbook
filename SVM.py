2f0acf9d6056d8f0cc4fc6fd9e507ff5eab18c43
123385918 <45360163+123385918@users.noreply.github.com>
2020-04-01 23:40:41 +0800

    Add files via upload

--- SVM.py
+++ SVM.py
@@ -15,20 +15,50 @@ class SVC:
                     np.flatnonzero(self.alpha==0)]
         return rst
 
-    def _kkt(self,i):
+    def _g(self,i):
+        '''
+        g(i) = Σaj*yj*xj*xi+b
+        '''
+        return (self.alpha * self.y).dot(self.Gram[:,i]) + self.b
+
+    def _kkt(self,i,g):
         '''判断以i为索引的样本点是否符合KKT条件'''
+        if self.alpha[i]==0:
+            return self.y[i]*g >= 1
+        elif self.alpha[i]==self.C:
+            return self.y[i]*g <= 1
+        else:
+            return self.y[i]*g == 1
         
-    def train(self,C,tol):
+    def train(self,C,tol=0.001):
         self.C = C
-        self.tol = tol
+        self.tol = tol ## 迭代精度，当|a_new-a_old|<e,停止迭代
         self.alpha = np.zeros(len(self.y)) ## 初始设置全部样本点都是非支持向量
         self.Gram = self.X.dot(self.X.T) ## 普通时用gram矩阵，加核时换成核矩阵
+        self.b = 0
+        self.E = np.array([self._g(i)-self.y[i] for i in range(len(self.y))])
 
-        ## 外层循环--第一个变量a1的选取
-        ## 首先遍历0<a1<C的样本点(间隔边界上的支持向量)，若均符合KKT条件，
-        ## 则继续找在a1=C的样本点(间隔边界内的支持向量+误分的支持向量)，若均符合KKT条件，
-        ## 则继续找在a1=0的样本点(非支持向量)，若均符合KKT条件，此时alpha列表即所求。
-        ids = self._reorder()
-        for i in ids:
+        while e > self.tol:
+
+            ## 外层循环--第一个变量a1的选取
+            ## 首先遍历0<a1<C的样本点(间隔边界上的支持向量)，若均符合KKT条件，
+            ## 则继续找在a1=C的样本点(间隔边界内的支持向量+误分的支持向量)，若均符合KKT条件，
+            ## 则继续找在a1=0的样本点(非支持向量)，若均符合KKT条件，此时alpha列表即所求。
+            for i in self._reorder():
+                
+                g = self._g(i)
+                if self._kkt(i,g)==False:
+                    a1 = self.alpha[i]
+                    E1 = self.E[i]
+                    j = self.E.argmax() if E1<0 else self.E.argmin()
+                    a2 = self.alpha[j]
+                    E2 = self.E[j]
+                    break
+            else:
+                self.w
+                self.b
+                return 'train done'
             
+            # eta=K11+K22-2K12
+            eta = self.Gram[i,i]+self.Gram[j,j]-2*self.Gram[i,j]
 
