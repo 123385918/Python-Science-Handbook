d6c69d51264689ec0f3e361dd93a122de27d3b5f
123385918 <45360163+123385918@users.noreply.github.com>
2020-06-28 00:10:26 +0800

    Update Kmean.py

--- Kmean.py
+++ Kmean.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
+
+dist = lambda X, centers : np.linalg.norm(X[None,:] - centers[:,None], axis=2) ## k*m
+agg = lambda keys,func:pd.DataFrame(X).groupby(keys).agg(func).values
+
+
 class Kmean:
     def __init__(self,X):
         self.X = X ## m*n
-        self.dist = lambda centers: np.linalg.norm(self.X[None,:] - centers[:,None], axis=2) ## k*m
-        self.agg = lambda keys,func:pd.DataFrame(self.X).groupby(keys).agg(func).values
         
     def train(self,k,centers=None):
-        self.maxiter = 1000
         
         ## 初始化类中心
-        if centers==None:
-            centers = self.X[np.random.choice(self.X.shape[0],k, replace=False)]
+        self.centers = np.array(centers or 
+                    self.X[np.random.choice(self.X.shape[0],k, replace=False)])
         
         ## 训练类中心
-        losses = []
-        while len(losses) < self.maxiter:
-            dist = self.dist(centers) ## k*m
-            cluster_id = dist.argmin(axis=0) ## m*1
-            loss = self.agg(cluster_id, lambda x: len(x)*x.var(ddof=0)).sum()
-            losses.append(loss)
-        else:
-            self.centers = self.agg(cluster_id, np.mean) ## k*m
-            return 'train done!'
+        self.losses = []
+        while True:
+            cluster_id = self.predict(self.X)
+            self.centers = agg(cluster_id, np.mean) ## k*m
+            loss = agg(cluster_id, lambda x: len(x)*x.var(ddof=0)).sum().round(4)
+            if self.losses and loss==self.losses[-1]: break
+            self.losses.append(loss)
+        return 'train done!'
         
-    def predict(self,X):
-        dist = np.linalg.norm(X[None,:] - self.centers[:,None], axis=2) ## k*m
-        cluster_id = dist.argmin(axis=0) ## m*1
+    def predict(self, X):
+        _dist = dist(X, self.centers) ## k*m
+        cluster_id = _dist.argmin(axis=0) ## m*1
         return cluster_id
+    
+if __name__=='__main__':
+    X = np.array([[0, 2], [0, 0], [1, 0], [5, 0], [5, 2]])
+    kmean = Kmean(X)
+    kmean.train(k=2)
+    kmean.losses
